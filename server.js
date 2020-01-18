4a9e997b7b24c2fc44a9b246986e084a1a0fb8af
RICHARD BARANOWSKI <a7018696@hallam.shu.ac.uk>
2020-01-18 17:10:41 +0000

    registration server server, no DB saving yet

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // server.js
 // load the things we need
 var express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 var app = express();
+const bcrypt = require('bcrypt');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -17,33 +18,58 @@ mongoose.connection.once('open', function(){
 
 //serve static files
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
 
+/*--------------------------------------------------
+ ____             _            
+|  _ \ ___  _   _| |_ ___  ___ 
+| |_) / _ \| | | | __/ _ \/ __|
+|  _ < (_) | |_| | ||  __/\__ \
+|_| \_\___/ \__,_|\__\___||___/             
+*/
 // use res.render to load up an ejs view file
-
 // index page 
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
-
 // files page 
 app.get('/files', function(req, res) {
     res.render('pages/files');
 });
-
 // users page 
 app.get('/users', function(req, res) {
     res.render('pages/users');
 });
-
 // sites page 
 app.get('/sites', function(req, res) {
     res.render('pages/sites');
 });
-
 // chat page 
 app.get('/chat', function(req, res) {
     res.render('pages/chat');
 });
+//login
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+app.post('/login', function(req, res) {
+    res.render('pages/login');
+});
+//register
+app.get('/register', function(req, res) {
+    res.render('pages/register');
+});
+app.post('/register', async function(req, res) {
+    try{
+        const hashedPasswword = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPasswword);
+        res.redirect('/login')
+    }catch{
+        res.redirect('/register')
+    }
+    console.log('errr')
+});
+/*------------------------------------------*/
 
 app.listen(8080);
 console.log('8080 is up');
